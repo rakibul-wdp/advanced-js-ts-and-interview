820e8ab78b57ffa064761f6499d4f09c58cd212d
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-14 04:15:20 +0600

    function call by template literale and it's a arguments

--- js-interview/js-tips-tricks/three/script.js
+++ js-interview/js-tips-tricks/three/script.js
@@ -1,6 +1,19 @@
+// 1
 // what will be the output of the below code?
-console.log([] + []); // ""
+// console.log([] + []); // ""
 // at first empty array convert empty string then compare and result will empty string.
 
-console.log({} + []);
+// console.log({} + []);
 // first convert String({}) -> [object Object], String([]) -> ""
+
+// 2
+// what will be the output of below code?
+function myFunction() {
+  // console.log(arguments);
+  // return arguments[0] + "Bangladesh";
+
+  return "Bangladesh";
+}
+
+const string = myFunction `hello `;
+console.log(string);
