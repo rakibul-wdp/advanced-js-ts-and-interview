e3080b67b81472b2bbade957358055ec32a2391d
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-17 04:25:20 +0600

    compare two same array index or element

--- js-interview/top-100/top100.js
+++ js-interview/top-100/top100.js
@@ -9,10 +9,19 @@
 // memory location compare and memory location difference
 
 // 2
-let a = [];
-let b = a;
+// let a = [];
+// let b = a;
 
-console.log(a == b);
-console.log(a === b);
+// console.log(a == b);
+// console.log(a === b);
 
 // memory location same
+
+// 3
+let a = [20];
+let b = [20];
+
+console.log(a[0] == b[0]);
+console.log(a[0] === b[0]);
+
+// here not compare memory location, here just compare index value
