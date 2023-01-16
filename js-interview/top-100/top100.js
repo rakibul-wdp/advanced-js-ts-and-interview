24960e618cdd6816f162d3d11113914af4886676
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-17 04:15:20 +0600

    compare two same array and it is true because memory location same

--- js-interview/top-100/top100.js
+++ js-interview/top-100/top100.js
@@ -1,9 +1,18 @@
 // 1
-let a = [];
-let b = [];
+// let a = [];
+// let b = [];
 
-console.log([1] === [1]);
+// console.log([1] === [1]);
+// console.log(a == b);
+// console.log(a === b);
+
+// memory location compare and memory location difference
+
+// 2
+let a = [];
+let b = a;
+
 console.log(a == b);
 console.log(a === b);
 
-// memory location compare and memory location difference
+// memory location same
