61c2bcdb056f5bc8786ab2342ccc040af059bb11
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-23 18:58:40 +0600

    convert any value to boolean

---
+++ js-interview/two.js
@@ -0,0 +1,10 @@
+// Convert any value to boolean
+
+// Using !! in front of any value
+console.log(!!"Chandu");
+console.log(!!1);
+console.log(!!0);
+console.log(!!undefined);
+
+// We can also use Boolean() to achieve same
+console.log(Boolean("Chandu"));
