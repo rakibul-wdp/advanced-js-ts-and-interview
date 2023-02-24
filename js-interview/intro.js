d43c4ae65bf4cf17ca7a33efcb8f1754799a1f69
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-24 12:31:15 +0600

    intro and all topic

---
+++ js-interview/intro.js
@@ -0,0 +1,36 @@
+/**
+ * 1. how can we swap values?
+ * [1, 2, 3, 4, 5] -> [5, 2, 3, 4, 1]
+ */
+
+// 2. how to copy things from clipboard
+
+/**
+ * 3. destructuring aliases
+ * const language ={name: "JavaScript", founded: "1995", founder: "Brendan Eich"}
+ */
+
+/**
+ * 4. Get value as data type
+ * <input id="number" value="123" type="number" />
+ */
+
+/**
+ * 5. Remove duplicate from array
+ * [1, 2, 3, 5, 5] -> [1, 2, 5]
+ */
+
+/**
+ * 6. Compare two arrays by value
+ * compare([1, 3], [1, 5]) -> false
+ */
+
+/**
+ * 7. Shuffling an array
+ * [1, 2, 3, 4, 5] -> [2, 1, 4, 3, 5]
+ */
+
+/**
+ * 8. using comma operator
+ * x = (2, 3) -> what is the output of x here? 
+ */
