18a0412a95f2977b382e19861463e3d1d740b9b5
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-25 21:53:41 +0600

    value swap with example

--- js-interview/intro.js
+++ js-interview/intro.js
@@ -2,6 +2,27 @@
  * 1. how can we swap values?
  * [1, 2, 3, 4, 5] -> [5, 2, 3, 4, 1]
  */
+let array = [1, 2, 3, 4, 5];
+
+// temp variable
+// let temp = array[0];
+// array[0] = array[4];
+// array[4] = temp;
+
+// console.log(array);
+
+// array destructuring
+// [array[0], array[4]] = [array[4], array[0]];
+// console.log(array);
+
+let a = 1;
+let b = 2;
+// [a, b] = [b, a];
+// console.log(a, b);
+
+// math
+b = a + (a = b) - b;
+console.log(a, b);
 
 // 2. how to copy things from clipboard
 
