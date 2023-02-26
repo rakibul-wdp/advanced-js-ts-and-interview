c00f93bc334634ccfbdf0223fe2a196871e2e67c
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-26 14:35:18 +0600

    comma operator

--- js-interview/intro.js
+++ js-interview/intro.js
@@ -69,7 +69,7 @@
 /**
  * 7. Shuffling an array
  * [1, 2, 3, 4, 5] -> [2, 1, 4, 3, 5]
-*/
+ */
 
 // const numbers = [1, 2, 3, 4, 5, 6];
 
@@ -79,3 +79,17 @@
  * 8. using comma operator
  * x = (2, 3) -> what is the output of x here?
  */
+
+let x = 1;
+x = (x++, x);
+
+console.log(x);
+
+let y = (2, 3);
+console.log(y);
+
+let a = [[1, 2, 3, 4], [3, 4, 5], [5, 6], [7]];
+
+for (let i = 0, j = 3; i <= 3; i++, j--) {
+  console.log("a[" + i + "][" + j + "] = " + a[i][j]);
+}
