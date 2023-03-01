798ed81bf0871bd02f41b3fbe388e694f58c4795
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-01 18:05:19 +0600

    create debounce handler function

--- js-interview/js-tips-tricks/four/script.js
+++ js-interview/js-tips-tricks/four/script.js
@@ -1,5 +1,17 @@
 const button = document.getElementById("button");
 
-button.addEventListener("click", function () {
-  console.log("clicked");
-});
+// debounce handler
+function debounce(fn, delay) {
+  return function () {
+    setTimeout(() => {
+      fn();
+    }, delay);
+  };
+}
+
+button.addEventListener(
+  "click",
+  debounce(function () {
+    console.log("clicked");
+  }, 2000)
+);
