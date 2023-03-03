b4692bf50e040bccf970cc1b51668c0e7ecf7423
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-03 19:36:35 +0600

    fetch api

--- js-interview/js-tips-tricks/one/two.js
+++ js-interview/js-tips-tricks/one/two.js
@@ -1,10 +1,27 @@
 // Convert any value to boolean
 
 // Using !! in front of any value
-console.log(!!"Chandu");
-console.log(!!1);
-console.log(!!0);
-console.log(!!undefined);
+// console.log(!!"Chandu");
+// console.log(!!1);
+// console.log(!!0);
+// console.log(!!undefined);
 
-// We can also use Boolean() to achieve same
-console.log(Boolean("Chandu"));
+// // We can also use Boolean() to achieve same
+// console.log(Boolean("Chandu"));
+
+// fetch("https://openapi.programming-hero.com/api/ai/tools")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     // You can now use the data variable here
+//   })
+//   .catch((error) => console.error(error));
+
+fetch("https://openapi.programming-hero.com/api/ai/tools")
+  .then((response) => response.json())
+  .then((data) => {
+    const result = data.data.tools;
+    return result;
+  });
+
+console.log(result);
