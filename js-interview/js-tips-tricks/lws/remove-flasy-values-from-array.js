04bb607db512653e7b8744814b786e7d1fd225d7
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-06 12:39:45 +0600

    explanation of remove falsy value

--- js-interview/js-tips-tricks/lws/remove-flasy-values-from-array.js
+++ js-interview/js-tips-tricks/lws/remove-flasy-values-from-array.js
@@ -17,3 +17,19 @@ let miscellaneous = [
 let fruits = miscellaneous.filter(Boolean);
 
 console.log(fruits);
+
+// explanation
+// Boolean(expression) is JS return true/false
+Boolean(5 < 6); // true
+Boolean(100 > 200); // false
+Boolean("JavaScript"); // true
+Boolean(""); // false
+
+// array example
+let miscellaneous2 = ["apple", false, "orange", NaN];
+let fruits2 = miscellaneous2.filter(function (element) {
+  return false;
+});
+let fruits3 = miscellaneous2.filter(Boolean);
+
+console.log(fruits2, fruits3);
