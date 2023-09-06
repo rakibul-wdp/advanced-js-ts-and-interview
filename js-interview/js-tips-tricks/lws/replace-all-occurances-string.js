47d9e01b1453b7ab9aa285e80da448d5cebbcdcb
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-06 14:01:01 +0600

    replace all occurrences of a string

---
+++ js-interview/js-tips-tricks/lws/replace-all-occurances-string.js
@@ -0,0 +1,6 @@
+// replace all occurrences of a string
+const quote =
+  "React is JS framework & this framework is the most popular front-end framework right now";
+
+// replace all occurrences of "framework" with "library"
+console.log(quote.replace(/framework/g, "library"));
