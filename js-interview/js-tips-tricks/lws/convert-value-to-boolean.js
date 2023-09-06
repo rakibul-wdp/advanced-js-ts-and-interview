7dccca9da502075d0e7e23e16d673fb1cc052e56
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-06 12:43:56 +0600

    convert any value to boolean

---
+++ js-interview/js-tips-tricks/lws/convert-value-to-boolean.js
@@ -0,0 +1,10 @@
+// convert any value to boolean
+
+console.log("Abul"); // Abul
+console.log(!!"Abul"); // true
+console.log(!!1); // true
+console.log(!!0); // false
+console.log(!undefined); // false
+
+// we can also use Boolean() to achieve same
+console.log(Boolean("Abul")); // true
