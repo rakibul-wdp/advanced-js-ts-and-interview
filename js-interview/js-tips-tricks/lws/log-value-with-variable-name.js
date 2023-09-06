985cbbba35f97ebccef3a4107d925dcd83efe885
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-07 04:05:00 +0600

    log values with variable names smartly

---
+++ js-interview/js-tips-tricks/lws/log-value-with-variable-name.js
@@ -0,0 +1,11 @@
+// log values with variable names smartly
+const library1 = "jQuery";
+const library2 = "React";
+
+// Instead of doing this
+console.log(`library1 - ${library1}`);
+console.log(`library2 - ${library2}`);
+
+// we can do this
+console.log({ library1 });
+console.log({ library2 });
