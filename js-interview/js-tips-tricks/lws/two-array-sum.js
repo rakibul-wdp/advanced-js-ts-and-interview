e2485c8f3984a23e15ab237f4b221e22568b1824
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-10 05:05:00 +0600

    tagged template literale

--- js-interview/js-tips-tricks/lws/two-array-sum.js
+++ js-interview/js-tips-tricks/lws/two-array-sum.js
@@ -2,3 +2,10 @@
 console.log([] + []); // ""
 
 console.log({} + []); // [object object]
+
+function myFunction() {
+  return "Bangladesh";
+}
+
+const string = myFunction`hello `;
+console.log(string);
