21708d3033cc20dd3276bae7eebe400a01be40f1
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-11 04:55:00 +0600

    arguments dot length or this dot length

--- js-interview/js-tips-tricks/lws/two-array-sum.js
+++ js-interview/js-tips-tricks/lws/two-array-sum.js
@@ -11,3 +11,16 @@ const string = myFunction`hello `;
 console.log(string);
 
 // by document.body.contentEditable = true we can make whole website editable
+
+function b() {
+  console.log(`the length is ${this.length}`);
+}
+
+let a = {
+  length: 10,
+  method: function (b) {
+    arguments[0]();
+  },
+};
+
+a.method(b, 5);
