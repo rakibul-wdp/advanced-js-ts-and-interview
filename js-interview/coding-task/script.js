cae4be07ab938fbae782c4e7f521e5efe5df1e98
rakibul-wdp <rakibul.wdp@gmail.com>
2024-05-10 03:39:00 +0600

    temporal dad zone tdz

--- js-interview/coding-task/script.js
+++ js-interview/coding-task/script.js
@@ -1,5 +1,20 @@
-// 1
+/*
+1
+
 console.log("value of age is", age);
 var age = 20;
 console.log("value of age is", age);
-// hoisting
+
+hoisting
+*/
+
+/*
+2
+
+age = 100;
+console.log("value of age is", age);
+let age = 30;
+console.log("value of age is", age);
+
+temporal dead zone
+*/
