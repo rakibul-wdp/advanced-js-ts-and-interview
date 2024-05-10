b4c1635b61feb3de31f68811909979891a7fb421
rakibul-wdp <rakibul.wdp@gmail.com>
2024-05-11 02:39:00 +0600

    execution context and efi function

--- js-interview/coding-task/script.js
+++ js-interview/coding-task/script.js
@@ -32,5 +32,18 @@ function myFun() {
 }
 myFun();
 
-hoisting | execution contest
+hoisting | execution context
 */
+
+// 4
+var variable = 10;
+(() => {
+  console.log(variable);
+  variable = 20;
+  console.log(variable);
+})();
+console.log(variable);
+var variable = 30;
+console.log(variable);
+
+// 10 -> 20 -> 20 -> 30 | error(variable same)
