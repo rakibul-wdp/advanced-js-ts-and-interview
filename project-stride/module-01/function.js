86f5ef01777a9c9d6dea015d9ffd029b9a99b904
rakibul-wdp <rakibul.wdp@gmail.com>
2024-05-17 03:39:00 +0600

    obj value this normal fun and arrow fun

--- project-stride/module-01/function.js
+++ project-stride/module-01/function.js
@@ -9,7 +9,7 @@ const result = add(2, 3);
 const multiply = function (num1, num2) {
   return num1 * num2;
 };
-console.log(multiply(5, 7));
+// console.log(multiply(5, 7));
 
 // console.log(result);
 
@@ -18,7 +18,19 @@ console.log(multiply(5, 7));
 // };
 
 const sum = (num1, num2) => num1 + num2;
-
 const res = sum(3, 5);
+// console.log(res);
 
-console.log(res);
+// this
+let user = {
+  age: 45,
+  getAge: function () {
+    console.log("normal fun: ", this.age);
+  },
+  getAgeDetails: () => {
+    console.log("arrow fun: ", this.age);
+  },
+};
+
+const res1 = user.getAge();
+const res2 = user.getAgeDetails();
