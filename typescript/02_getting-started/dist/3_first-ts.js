d0d9be806bce40d4c28fe8f20872d8e7242f53d1
rakibul-wdp <rakibul.wdp@gmail.com>
2025-03-10 06:26:40 +0600

    enums

--- typescript/02_getting-started/dist/3_first-ts.js
+++ typescript/02_getting-started/dist/3_first-ts.js
@@ -1,14 +1,4 @@
 "use strict";
-let sales = 123456789;
-let course = "TypeScript";
-let isPublished = true;
-let level;
-level = 1;
-level = "a";
-function render(document) {
-    console.log(document);
-}
-let numbers = [];
-numbers.forEach((n) => n);
-let user = [1, "John"];
+let mySize = 2;
+console.log(mySize);
 //# sourceMappingURL=3_first-ts.js.map
--- typescript/02_getting-started/src/3_first-ts.ts
+++ typescript/02_getting-started/src/3_first-ts.ts
@@ -1,17 +1,12 @@
-let sales = 123_456_789;
-let course = "TypeScript";
-let isPublished = true;
-let level;
-level = 1;
-level = "a";
+// const small = 1;
+// const medium = 2;
+// const large = 3;
 
-function render(document: any) {
-  console.log(document);
+// PascalCase
+const enum Size {
+  Small = 1,
+  Medium,
+  Large,
 }
-
-let numbers: number[] = [];
-numbers.forEach((n) => n);
-
-// 1, "John"
-let user: [number, string] = [1, "John"];
-user.push(1);
+let mySize: Size = Size.Medium;
+console.log(mySize);
