79c8ed541908dc46600b39f8d15c423a22a4c9b7
rakibul-wdp <rakibul.wdp@gmail.com>
2025-03-10 10:14:33 +0600

    exercises

--- typescript/02_getting-started/src/3_first-ts.ts
+++ typescript/02_getting-started/src/3_first-ts.ts
@@ -1,11 +1,20 @@
-let employee: {
-  readonly id: number;
-  name: string;
-  retire: (date: Date) => void;
-} = {
-  id: 1,
-  name: "John",
-  retire: (date: Date) => {
-    console.log(date);
-  },
-};
+let a: number = 100;
+let b: string = "Coffee";
+let c: boolean[] = [true, false, false];
+let d: { age: number } = { age: 20 };
+let e: number[] = [3];
+let f: any;
+let g: any[] = [];
+
+// Property 'releaseYear' is missing in type '{ title: string; }' but required in type '{ title: string; releaseYear: number; }'
+let song: {
+  title: string;
+  releaseYear: number;
+} = { title: "My song" };
+
+// Type 'string' is not assignable to type 'number'.
+let prices = [100, 200, 300];
+prices[0] = "$100";
+
+// A function whose declared type is neither 'undefined', 'void', nor 'any' must return a value.
+function myFunc(a: number, b: number): number {}
