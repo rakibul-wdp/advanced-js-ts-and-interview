082f8359ca3f324a0434507ece4582fc31adb52e
rakibul-wdp <rakibul.wdp@gmail.com>
2025-03-11 11:18:44 +0600

    exercise

--- typescript/02_getting-started/src/3_first-ts.ts
+++ typescript/02_getting-started/src/3_first-ts.ts
@@ -8,14 +8,5 @@
 //   document.whateverWeWant();
 // }
 
-// Birds fly. Fish swim. A pet can be a Bird or Fish. Use type aliases to represent these
-
-type Bird = {
-  fly: () => void;
-};
-
-type Fish = {
-  swim: () => void;
-};
-
-type Pet = Bird | Fish;
+let value: unknown = "a";
+if (typeof value === "string") console.log(value.toUpperCase());
