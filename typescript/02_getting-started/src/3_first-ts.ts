2276925b2c105165e4760e9e8f55d63f0c0daf82
rakibul-wdp <rakibul.wdp@gmail.com>
2025-03-11 11:08:44 +0600

    define user type

--- typescript/02_getting-started/src/3_first-ts.ts
+++ typescript/02_getting-started/src/3_first-ts.ts
@@ -1,9 +1,25 @@
-function render(document: unknow) {
-  // Narrowing
-  if (document instanceof WordDocument) {
-    document.toUpperCase();
-  }
-  document.move();
-  document.fly();
-  document.whateverWeWant();
-}
+// function render(document: unknow) {
+//   // Narrowing
+//   if (document instanceof WordDocument) {
+//     document.toUpperCase();
+//   }
+//   document.move();
+//   document.fly();
+//   document.whateverWeWant();
+// }
+type IUser = {
+  name: string;
+  age: number;
+  occupation?: string;
+};
+let users: IUser[] = [
+  {
+    name: "John Smith",
+    age: 30,
+    occupation: "Software engineer",
+  },
+  {
+    name: "Kate Muller",
+    age: 28,
+  },
+];
