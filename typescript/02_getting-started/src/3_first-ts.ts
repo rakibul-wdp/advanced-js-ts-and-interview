7033b1e19c2db94b1f292f063bfa524ebb4f680d
rakibul-wdp <rakibul.wdp@gmail.com>
2025-03-11 10:54:27 +0600

    type assertions

--- typescript/02_getting-started/src/3_first-ts.ts
+++ typescript/02_getting-started/src/3_first-ts.ts
@@ -1,6 +1,4 @@
-let speed: number | null = null;
-let ride = {
-  // Falsy (undefined, null, "", false, 0)
-  // Nullish coalescing operator
-  speed: speed ?? 30,
-};
+let phone = <HTMLInputElement>document.getElementById("phone");
+// HTMLElement
+// HTMLInputElement
+phone.value;
