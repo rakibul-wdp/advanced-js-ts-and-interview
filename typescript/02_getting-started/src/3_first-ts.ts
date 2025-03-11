af89e1fa5112480a397f20493ba42f1c7067b474
rakibul-wdp <rakibul.wdp@gmail.com>
2025-03-11 10:45:40 +0600

    optional chaining

--- typescript/02_getting-started/src/3_first-ts.ts
+++ typescript/02_getting-started/src/3_first-ts.ts
@@ -1,6 +1,17 @@
-function greet(name: string | null | undefined) {
-  if (name) console.log(name.toUpperCase());
-  else console.log("Hola!");
+type Customer = {
+  birthday?: Date;
+};
+
+function getCustomer(id: number): Customer | null {
+  return id === 0 ? null : { birthday: new Date() };
 }
 
-greet(undefined);
+let customer = getCustomer(0);
+console.log(customer?.birthday?.getFullYear);
+
+// Optional element access operator
+// customers?.[0]
+
+// Optional call
+let log: any = null;
+log?.("a");
