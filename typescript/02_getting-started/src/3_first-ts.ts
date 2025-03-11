8686de9db1e11aca357a529c11b8f8b4d21e3552
rakibul-wdp <rakibul.wdp@gmail.com>
2025-03-11 06:19:15 +0600

    type aliases

--- typescript/02_getting-started/src/3_first-ts.ts
+++ typescript/02_getting-started/src/3_first-ts.ts
@@ -1,9 +1,13 @@
-// Advance Types
-/*
-- Type aliases
-- Unions and intersections
-- Type narrowing
-- Nullable types
-- The unknown type
-- The never type
-*/
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+
+let employee: Employee = {
+  id: 1,
+  name: "John",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
