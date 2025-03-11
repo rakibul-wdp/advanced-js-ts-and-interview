5fe5f48fea67cfdfa31c47f134d2e99a7d14fe93
rakibul-wdp <rakibul.wdp@gmail.com>
2025-03-11 06:13:50 +0600

    advance types

--- typescript/02_getting-started/src/3_first-ts.ts
+++ typescript/02_getting-started/src/3_first-ts.ts
@@ -1,20 +1,9 @@
-let a: number = 100;
-let b: string = "Coffee";
-let c: boolean[] = [true, false, false];
-let d: { age: number } = { age: 20 };
-let e: number[] = [3];
-let f: any;
-let g: any[] = [];
-
-// Property 'releaseYear' is missing in type '{ title: string; }' but required in type '{ title: string; releaseYear: number; }'
-let song: {
-  title: string;
-  releaseYear: number;
-} = { title: "My song" };
-
-// Type 'string' is not assignable to type 'number'.
-let prices = [100, 200, 300];
-prices[0] = "$100";
-
-// A function whose declared type is neither 'undefined', 'void', nor 'any' must return a value.
-function myFunc(a: number, b: number): number {}
+// Advance Types
+/*
+- Type aliases
+- Unions and intersections
+- Type narrowing
+- Nullable types
+- The unknown type
+- The never type
+*/
