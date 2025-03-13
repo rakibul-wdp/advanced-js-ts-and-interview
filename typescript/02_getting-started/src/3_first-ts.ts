c904bee60661fe3e21ae2545b3f1b01f1f73f42d
rakibul-wdp <rakibul.wdp@gmail.com>
2025-03-13 09:07:59 +0600

    index signatures

--- typescript/02_getting-started/src/3_first-ts.ts
+++ typescript/02_getting-started/src/3_first-ts.ts
@@ -1,24 +1,7 @@
-class Account {
-  nickname?: string;
-
-  constructor(
-    public readonly id: number,
-    public owner: string,
-    private _balance: number
-  ) {}
-
-  deposit(amount: number): void {
-    if (amount <= 0) throw new Error("Invalid amount");
-    // Record a transaction
-    this._balance += amount;
-  }
-
-  private calculateTax() {}
-
-  get balance(): number {
-    return this._balance;
-  }
+class SeatAssignment {
+  [seatNumber: string]: string;
 }
 
-let account = new Account(1, "John", 0);
-console.log(account.balance);
+let seats = new SeatAssignment();
+seats.A1 = "John";
+seats.A2 = "Doe";
