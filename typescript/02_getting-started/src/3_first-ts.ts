f7706306487d25073343a7a646e4ef2377bf79ce
rakibul-wdp <rakibul.wdp@gmail.com>
2025-03-13 08:55:07 +0600

    access control keywords

--- typescript/02_getting-started/src/3_first-ts.ts
+++ typescript/02_getting-started/src/3_first-ts.ts
@@ -1,19 +1,27 @@
 class Account {
   readonly id: number;
   owner: string;
-  balance: number;
+  private _balance: number;
   nickname?: string;
 
   constructor(id: number, owner: string, balance: number) {
     this.id = id;
     this.owner = owner;
-    this.balance = balance;
+    this._balance = balance;
   }
 
   deposit(amount: number): void {
     if (amount <= 0) throw new Error("Invalid amount");
-    this.balance += amount;
+    // Record a transaction
+    this._balance += amount;
+  }
+
+  private calculateTax() {}
+
+  getBalance(): number {
+    return this._balance;
   }
 }
 
 let account = new Account(1, "John", 0);
+console.log(account.getBalance());
