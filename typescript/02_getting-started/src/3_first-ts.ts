6a9bdba042649670ff7dc38d78a752fc9dc342cb
rakibul-wdp <rakibul.wdp@gmail.com>
2025-04-01 08:29:51 +0600

    class decorators

--- typescript/02_getting-started/src/3_first-ts.ts
+++ typescript/02_getting-started/src/3_first-ts.ts
@@ -1,21 +1,7 @@
-interface Product {
-  name: string;
-  price: number;
+function Component(constructor: Function) {
+  console.log("Component decorator called");
+  constructor.prototype.uniqueId = Date.now();
+  constructor.prototype.insertInDOM = () => {
+    console.log("Inserting the component in the DOM");
+  };
 }
-
-type ReadOnly<T> = {
-  readonly [K in keyof T]: T[K];
-};
-
-type Optional<T> = {
-  [K in keyof T]?: T[K];
-};
-
-type Nullable<T> = {
-  [K in keyof T]: T[K] | null;
-};
-
-let product: ReadOnly<Product> = {
-  name: "a",
-  price: 1,
-};
