ca77e19823f8ebb9d9f3b599018133a081b59de4
rakibul-wdp <rakibul.wdp@gmail.com>
2025-04-01 08:35:40 +0600

    parameterized decorators

--- typescript/02_getting-started/src/3_first-ts.ts
+++ typescript/02_getting-started/src/3_first-ts.ts
@@ -1,7 +1,17 @@
-function Component(constructor: Function) {
-  console.log("Component decorator called");
-  constructor.prototype.uniqueId = Date.now();
-  constructor.prototype.insertInDOM = () => {
-    console.log("Inserting the component in the DOM");
+type ComponentOptions = {
+  selector: string;
+};
+
+function Component(options: ComponentOptions) {
+  return (constructor: Function) => {
+    console.log("Component decorator called");
+    constructor.prototype.options = options;
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.insertInDOM = () => {
+      console.log("Inserting the component in the DOM");
+    };
   };
 }
+
+@Component({ selector: "#my-profile" })
+class ProfileComponent {}
