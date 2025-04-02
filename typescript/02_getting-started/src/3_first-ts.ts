5078787bdeb52164aa5f6d7cc576adc0267c2687
rakibul-wdp <rakibul.wdp@gmail.com>
2025-04-02 06:51:46 +0600

    decorator composition

--- typescript/02_getting-started/src/3_first-ts.ts
+++ typescript/02_getting-started/src/3_first-ts.ts
@@ -1,17 +1,9 @@
-type ComponentOptions = {
-  selector: string;
-};
-
-function Component(options: ComponentOptions) {
-  return (constructor: Function) => {
-    console.log("Component decorator called");
-    constructor.prototype.options = options;
-    constructor.prototype.uniqueId = Date.now();
-    constructor.prototype.insertInDOM = () => {
-      console.log("Inserting the component in the DOM");
-    };
-  };
+function Pipe(constructor: Function) {
+  console.log("Pipe decorator called");
+  constructor.prototype.pipe = true;
 }
 
 @Component({ selector: "#my-profile" })
+@Pipe
+// f(g(x))
 class ProfileComponent {}
