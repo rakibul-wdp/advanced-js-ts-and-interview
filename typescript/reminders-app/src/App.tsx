f630f28b1175e573e2898db36e7de2a96f7ce5cb
rakibul-wdp <rakibul.wdp@gmail.com>
2025-04-03 11:13:25 +0600

    handle use state with props

--- typescript/reminders-app/src/App.tsx
+++ typescript/reminders-app/src/App.tsx
@@ -1,14 +1,15 @@
+import { useState } from "react";
 import ReminderList from "./components/ReminderList";
 import Reminder from "./types/reminder";
 
-const reminders: Reminder[] = [
-  {
-    id: 1,
-    title: "Reminder 1",
-  },
-];
-
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([
+    {
+      id: 1,
+      title: "Reminder 1",
+    },
+  ]);
+
   return (
     <div>
       <ReminderList items={reminders} />
